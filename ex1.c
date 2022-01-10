cfb152c7540ede5bad722de140bb25bba8129d94
taramagar63 <sonamknicks@Taras-MacBook-Pro.local>
2022-01-10 07:18:24 +0545

    first commit

---
+++ ex1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    printf("\n%-9s BCA","2019");
+    printf("\n%-8s BCA","2019");
+    printf("\n%-7s BCA","2019");
+    printf("\n%-6s BCA","2019");
+    printf("\n%-5s BCA","2019");
+    printf("\n%-4s BCA","2019");
+    return 0;
+}
